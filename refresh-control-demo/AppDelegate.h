556ff6ee72c08116830497676695d2d3b068aa01
CavinTang <songzhu1988@qq.com>
2016-01-28 15:38:20 +0800

    Initial Commit

---
+++ refresh-control-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  refresh-control-demo
+//
+//  Created by CavinTang on 1/28/16.
+//  Copyright © 2016 Cavin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
